aa0676e14ebcbab7f55c0b45e9e0e6cb099b64c2
celuk <seyyid4091@gmail.com>
2023-09-20 14:07:31 +0300

    add python script to boot linux

---
+++ riscv_linux.py
@@ -0,0 +1,72 @@
+import m5
+from m5.objects import Root
+
+from gem5.utils.requires import requires
+from gem5.components.boards.riscv_board import RiscvBoard
+from gem5.components.memory import DualChannelDDR4_2400
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.isas import ISA
+from gem5.simulate.simulator import Simulator
+from gem5.resources.workload import Workload
+
+from gem5.resources.workload import CustomWorkload
+from gem5.resources.resource import AbstractResource
+from gem5.resources.resource import CustomResource, CustomDiskImageResource, DiskImageResource
+
+# This runs a check to ensure the gem5 binary is compiled for RISCV.
+
+requires(isa_required=ISA.RISCV)
+
+# With RISCV, we use simple caches.
+from gem5.components.cachehierarchies.classic.private_l1_private_l2_cache_hierarchy import (
+    PrivateL1PrivateL2CacheHierarchy,
+)
+
+# Here we setup the parameters of the l1 and l2 caches.
+cache_hierarchy = PrivateL1PrivateL2CacheHierarchy(
+    l1d_size="16kB", l1i_size="16kB", l2_size="256kB"
+)
+
+# Memory: Dual Channel DDR4 2400 DRAM device.
+
+memory = DualChannelDDR4_2400(size="3GB")
+
+# Here we setup the processor. We use a simple processor.
+processor = SimpleProcessor(
+    cpu_type=CPUTypes.TIMING, isa=ISA.RISCV, num_cores=2
+)
+
+# Here we setup the board. The RiscvBoard allows for Full-System RISCV
+# simulations.
+board = RiscvBoard(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+# Here we a full system workload: "riscv-ubuntu-20.04-boot" which boots
+# Ubuntu 20.04. Once the system successfully boots it encounters an `m5_exit`
+# instruction which stops the simulation. When the simulation has ended you may
+# inspect `m5out/system.pc.com_1.device` to see the stdout.
+
+#board.set_workload(CustomResource(local_path="/home/shc/projects/riscv-imgs/ubuntudene/ubuntu-22.04.3-preinstalled-server-riscv64+unmatched.img"))
+
+#board.set_kernel_disk_workload(kernel=CustomResource("/home/shc/projects/riscv-imgs/ubuntu/bootloader-vmlinux-5.10"), disk_image=CustomDiskImageResource("/home/shc/projects/riscv-imgs/ubuntudene/ubuntu-22.04.3-preinstalled-server-riscv64+unmatched.img"))
+
+board.set_workload(CustomWorkload(
+    function = "set_kernel_disk_workload",
+    parameters = {
+        #"bootloader" : AbstractResource("/home/shc/projects/riscv-imgs/ubuntu/bootloader-vmlinux-5.10"),
+        "kernel" : CustomResource("/home/shc/projects/riscv-imgs/gem5cached/riscv-bootloader-vmlinux-5.10"),
+        "disk_image" : CustomDiskImageResource("/home/shc/projects/riscv-imgs/gem5cached/riscv-ubuntu-20.04-img")
+        #"disk_image" : DiskImageResource("/home/shc/projects/riscv-imgs/ubuntudene/ubuntu-22.04.3-preinstalled-server-riscv64+unmatched.img")
+    }
+))
+
+#board.set_workload(AbstractResource("/home/shc/projects/riscv-imgs/ubuntudene/ubuntu-22.04.3-preinstalled-server-riscv64+unmatched.img"))
+
+simulator = Simulator(board=board)
+simulator.run()
+
